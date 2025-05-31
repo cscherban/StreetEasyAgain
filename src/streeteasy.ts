653fc47510e613210290b3830561e5143673446d
Christopher Scherban <christopher.scherban@gmail.com>
2025-05-31 00:35:01 -0400

    match based on appartment name

--- src/streeteasy.ts
+++ src/streeteasy.ts
@@ -1,26 +1,123 @@
+type ApartmentData = {
+    address?: string
+    price?: string
+    rentOrSale?: string
+    feeInfo?: string
+    squareFootage?: string
+    pricePerFt?: string
+    rooms?: string
+    beds?: string
+    baths?: string
+    neighborhood?: string
+    contacted?: Date
+}
+
 function checkForTourButton() {
     const buttons = document.querySelectorAll('button')
     const filteredButtons = Array.prototype.filter.call(buttons, function (el) {
         return el.textContent.trim().toLowerCase() === 'request a tour'
     })
 
-    if (filteredButtons.length > 0) {
+    const slug = location.pathname.replace(/^\/building\//, '')
+
+    if (slug === '') {
+        console.log('Not a building page, exiting')
+        return false
+    }
+
+    const address = document
+        .querySelector('[data-testid="address"]')
+        ?.textContent?.trim()
+
+    // Price
+    const price = document
+        .querySelector('[data-testid="priceInfo"] h4')
+        ?.textContent?.trim()
+
+    // Rent/Sale indicator
+    const rentOrSale = document
+        .querySelector('[data-testid="priceInfo"] p')
+        ?.textContent?.trim()
+
+    // Fee info (e.g., "No fee")
+    const feeInfo = document
+        .querySelector('[data-testid="pill-noFee"] span')
+        ?.textContent?.trim()
+
+    // Square footage
+    const squareFootage = document
+        .querySelectorAll(
+            '[data-testid="propertyDetails"] .PropertyDetails_item__4mGTQ'
+        )[0]
+        ?.textContent?.trim()
+
+    // Price per ft²
+    const pricePerFt = document
+        .querySelectorAll(
+            '[data-testid="propertyDetails"] .PropertyDetails_item__4mGTQ'
+        )[1]
+        ?.textContent?.trim()
+
+    // Rooms
+    const rooms = document
+        .querySelectorAll(
+            '[data-testid="propertyDetails"] .PropertyDetails_item__4mGTQ'
+        )[2]
+        ?.textContent?.trim()
+
+    // Bedrooms
+    const beds = document
+        .querySelectorAll(
+            '[data-testid="propertyDetails"] .PropertyDetails_item__4mGTQ'
+        )[3]
+        ?.textContent?.trim()
+
+    // Bathrooms
+    const baths = document
+        .querySelectorAll(
+            '[data-testid="propertyDetails"] .PropertyDetails_item__4mGTQ'
+        )[4]
+        ?.textContent?.trim()
+
+    // Neighborhood
+    const neighborhood = document
+        .querySelector('[data-testid="buildingSummaryList"] a')
+        ?.textContent?.trim()
+
+    const appartmentData: ApartmentData = {
+        address,
+        price,
+        rentOrSale,
+        feeInfo,
+        squareFootage,
+        pricePerFt,
+        rooms,
+        beds,
+        baths,
+        neighborhood,
+    }
+
+    const appartmentName = address ?? slug
+
+    if (appartmentName && filteredButtons.length > 0) {
         const tourButton = filteredButtons[0]
         console.log('Found the "Request a tour" button')
 
         tourButton.addEventListener('click', function () {
+            appartmentData.contacted = new Date()
             chrome.storage.sync.set(
-                { apartment: { contacted: 'test' } },
+                { [appartmentName]: appartmentData },
                 function () {
                     console.log('Saved to storage')
                 }
             )
         })
 
-        chrome.storage.sync.get(['apartment'], function (items) {
-            console.log('Apartment data', items.apartment)
-            if (items.apartment?.contacted) {
-                console.log('Already contacted:', items.apartment.contacted)
+        chrome.storage.sync.get([appartmentName], function (items) {
+            console.log('Apartment data', items[appartmentName])
+            const data = items[appartmentName]
+            if (data?.contacted) {
+                console.log('Already contacted:', data)
                 tourButton.innerHTML = 'Contact Again!'
             }
         })
